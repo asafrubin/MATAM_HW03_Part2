39889b29018392f82e5fc5645e68a7ec22205227
asafrubin <asafrub@campus.technion.ac.il>
2017-06-05 12:05:35 +0300

    pp

--- Escapers.c
+++ Escapers.c
@@ -1,8 +1,39 @@
 #include "Escapers.h"
 #include "EscapeTechnion.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 
 struct SEscapers{
     char *email;
     TechnionFaculty faculty;
     int skill_level;
 };
+
+//free the inside of the escaper
+void *freeEscaper(Escapers *escaper)
+{
+    free(escaper->email);
+    escaper->email = NULL;
+}
+
+//copies an escaper element and returns a pointer to a new element
+Escapers *copyElement(Escapers *escaper)
+{
+    Escapers *escaperCopy = malloc( sizeof(*escaperCopy) );
+    if(escaperCopy == NULL){
+        return NULL;
+    }
+
+    escaperCopy->email = malloc( strlen(escaper->email) + 1);
+    if(escaperCopy->email == NULL){
+        free(escaperCopy);
+        return NULL;
+    }
+    strcpy(escaperCopy->email, escaper->email);
+
+    escaperCopy->faculty = escaper->faculty;
+    escaperCopy->skill_level = escaper->skill_level;
+
+    return escaperCopy;
+}
--- Escapers.h
+++ Escapers.h
@@ -3,4 +3,22 @@
 
 typedef struct SEscapers Escapers;
 
+typedef enum {
+    MTM_OUT_OF_MEMORY, // You should exit program after this error
+    MTM_INVALID_COMMAND_LINE_PARAMETERS, // You should exit program after this error
+    MTM_CANNOT_OPEN_FILE, // You should exit program after this error
+    MTM_NULL_PARAMETER,
+    MTM_INVALID_PARAMETER,
+    MTM_EMAIL_ALREADY_EXISTS,
+    MTM_COMPANY_EMAIL_DOES_NOT_EXIST,
+    MTM_CLIENT_EMAIL_DOES_NOT_EXIST,
+    MTM_ID_ALREADY_EXIST,
+    MTM_ID_DOES_NOT_EXIST,
+    MTM_CLIENT_IN_ROOM,
+    MTM_ROOM_NOT_AVAILABLE,
+    MTM_RESERVATION_EXISTS,
+    MTM_NO_ROOMS_AVAILABLE,
+    ESCAPER_SUCCESS,
+} EscapersErrorCode;
+
 #endif //HW03_PART2_ESCAPERS_H
