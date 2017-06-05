95a32d4b78e5f12e279712917826fa78da3c4060
nateRot <nate.technion@gmail.com>
2017-06-05 12:10:06 +0300

    rooms

--- Rooms.c
+++ Rooms.c
@@ -1,6 +1,7 @@
 #include "Rooms.h"
+#include <string.h>
 
-struct SRooms{
+typedef struct SRooms{
     char *email;
     int id;
     int price;
@@ -8,5 +9,27 @@ struct SRooms{
     int time_start;
     int time_finish;
     int difficulty;
-    Companies *company;
-};
+//    Companies *company;
+} Room;
+
+
+//requires that PARSER pass on deconstucted working_hrs
+RoomResult createRoom(char *email , int id , int price , int num_ppl , int time_start , int time_finish , int difficulty , Room *newRoom)
+{
+    Room *newRoom = malloc(sizeof(Room));
+    if (newRoom == NULL){
+        return ROOM_OUT_OF_MEMORY;
+    }
+    newRoom->email = malloc(strlen(email));
+    if (newRoom->email == NULL){
+        return ROOM_OUT_OF_MEMORY;
+    }
+    strcpy(newRoom->email , email);
+    newRoom->id = id;
+    newRoom->price = price;
+    newRoom->num_of_ppl = num_ppl;
+    newRoom->time_start = time_start;
+    newRoom->time_finish = time_finish;
+    newRoom->difficulty = difficulty;
+}
+
--- Rooms.h
+++ Rooms.h
@@ -5,6 +5,27 @@
 
 typedef struct SRooms Rooms;
 
+typedef enum roomResult_t {
+    //SET_SUCCESS,
+    ROOM_OUT_OF_MEMORY,
+    ROOM_NULL_ARGUMENT,
+    //SET_ITEM_ALREADY_EXISTS,
+    //SET_ITEM_DOES_NOT_EXIST
 
+} RoomResult;
+
+RoomResult createRoom();
+
+RoomResult freeRoom();
+
+RoomResult copyRoom();
+
+RoomResult checkAvailable();
+
+RoomResult addEscaperToRoom();
+
+RoomResult sortRoom();
+
+RoomResult
 
 #endif //HW03_PART2_ROOMS_H
