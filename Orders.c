90d812af08b4fff15d037487c94e2d7adbd4ab5a
nateRot <nate.technion@gmail.com>
2017-06-06 23:29:23 +0300

    rooms+orders

--- Orders.c
+++ Orders.c
@@ -11,11 +11,11 @@ typedef struct SOrders{
     int effective_ppl;
     int req_hour;
     int req_day;
-    bool discount;
+    int price;
 } Order;
 
 orderResult createOrder(char* escaperEmail , char *facultyEmail , int roomID , int req_num_of_ppl , int effective_ppl ,
-                        int req_hour , int req_day , bool discount , Order *newOrder)
+                        int req_hour , int req_day , int price , Order *newOrder)
 {
     newOrder = malloc(sizeof(Order));
     if(newOrder == NULL){
@@ -38,7 +38,7 @@ orderResult createOrder(char* escaperEmail , char *facultyEmail , int roomID , i
     newOrder->effective_ppl = effective_ppl;
     newOrder->req_hour = req_hour;
     newOrder->req_day = req_day;
-    newOrder->discount = discount;
+    newOrder->price = price;
     return ORDERS_SUCCESS;
 }
 
@@ -48,11 +48,37 @@ Order* copyOrder(Order order_to_copy)
     if(copy_of_order == NULL){
         return ORDERS_OUT_OF_MEMORY;
     }
-    copy_of_order->escaperEmail = malloc(strlen(order_to_copy.escaperEmail));
+    copy_of_order->escaperEmail = malloc(strlen(order_to_copy.escaperEmail) + 1);
     if(copy_of_order->escaperEmail == NULL){
         free(copy_of_order);
         return ORDERS_OUT_OF_MEMORY;
     }
     strcpy(copy_of_order->escaperEmail , order_to_copy.escaperEmail);
+    copy_of_order->facultyEmail = malloc(strlen(order_to_copy.facultyEmail) + 1);
+    if(copy_of_order->facultyEmail == NULL){
+        free(copy_of_order->escaperEmail);
+        free(copy_of_order);
+        return ORDERS_OUT_OF_MEMORY;
+    }
+    strcpy(copy_of_order->facultyEmail , order_to_copy.facultyEmail);
+    copy_of_order->roomID = order_to_copy.roomID;
+    copy_of_order->req_num_of_ppl = order_to_copy.req_num_of_ppl;
+    copy_of_order->effective_ppl = order_to_copy.effective_ppl;
+    copy_of_order->req_hour = order_to_copy.req_hour;
+    copy_of_order->req_day = order_to_copy.req_day;
+    copy_of_order->price = order_to_copy.price;
+    return copy_of_order;
+}
 
+orderResult removeOrder(Order *order)
+{
+    free(order->facultyEmail);
+    free(order->escaperEmail);
+    free(order);
+    return ORDERS_SUCCESS;
+}
+
+int getPrice(Order *order)
+{
+    return order->price;
 }
